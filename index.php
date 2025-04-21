72340ad784eb5eb4816260295e2c997a5d1ebc41
Alex Lemos <alexlemos_23@outlook.com>
2025-04-21 17:40:05 -0300

    Update index.php
    Este script PHP é uma aplicação web simples desenvolvida para testar a conectividade com um banco de dados MySQL, registrar informações fictícias e exibir a versão atual do PHP. Ele é especialmente útil para validar ambientes em containers Docker com Apache/Nginx, PHP e MySQL.

--- index.php
+++ index.php
@@ -1,48 +1,27 @@
 <html>
-
 <head>
-<title>Exemplo PHP</title>
+  <title>Exemplo PHP</title>
 </head>
 <body>
 
 <?php
+// Exibe erros PHP (ótimo para desenvolvimento)
 ini_set("display_errors", 1);
+
+// Define o charset para ISO-8859-1
 header('Content-Type: text/html; charset=iso-8859-1');
 
+// Mostra a versão do PHP
+echo 'Versão Atual do PHP: ' . phpversion() . '<br>';
 
-
-echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
-
+// Configurações do banco
 $servername = "54.234.153.24";
-$username = "root";
-$password = "Senha123";
-$database = "meubanco";
+$username   = "root";
+$password   = "Senha123";
+$database   = "meubanco";
 
-// Criar conexão
-
-
+// Conecta com o banco de dados usando MySQLi
 $link = new mysqli($servername, $username, $password, $database);
 
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-
-$valor_rand1 =  rand(1, 999);
-$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
-$host_name = gethostname();
-
-
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
-
-
-if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $link->error;
-}
-
-?>
-</body>
-</html>
+// Verifica conexão
+if ($link->connect_error_
